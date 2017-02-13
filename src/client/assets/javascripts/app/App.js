00002b9c4550a784208bcf5592318015db3188b7
Brian Nydegger <briannydegger@gmail.com>
2017-02-13 16:38:44 +0100

    feat(topbar): modifie le style de la bar

--- src/client/assets/javascripts/app/App.js
+++ src/client/assets/javascripts/app/App.js
@@ -1,10 +1,27 @@
 import React, { PropTypes } from 'react';
-import { Layout } from 'antd';
+import { Layout, Menu, Dropdown, Button, Icon } from 'antd';
+import { Col } from 'antd';
+import { Link } from 'react-router';
 const { Header } = Layout;
+
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <Link to="/login">Logout <Icon type="logout" /></Link>
+    </Menu.Item>
+  </Menu>
+);
 
 const App = (props) => (
   <Layout style={{height:'100%'}}>
-    <Header>My Access Web</Header>
+    <Header style={{backgroundColor: '#eee'}}>
+      <Col span={23}>My Access Web</Col>
+      <Col span={1}>
+        <Dropdown overlay={menu} placement="bottomRight">
+          <Button type="primary" shape="circle" icon="user" size="large" />
+        </Dropdown>
+      </Col>
+    </Header>
     {props.children}
   </Layout>
 );
