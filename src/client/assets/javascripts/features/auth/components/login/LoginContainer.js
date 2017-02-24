54f60ff40335077f3bee6146360f21d1b5ad243d
Benjamin Ganty <benjaminganty@msn.com>
2017-02-24 19:38:11 +0100

    feat(login): implémente les intéractions avec le formulaire de login

--- src/client/assets/javascripts/features/auth/components/login/LoginContainer.js
+++ src/client/assets/javascripts/features/auth/components/login/LoginContainer.js
@@ -1,20 +1,93 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { withRouter } from 'react-router';
 
-import { LoginFormComponent } from './LoginForm';
+import { actionCreators as authActions, selector } from '../../';
+import LoginForm from './LoginForm';
 
 import './Login.scss';
 
+@connect(selector, (dispatch) => ({
+  actions: bindActionCreators(authActions, dispatch)
+}))
+@withRouter
 export default class LoginContainer extends Component {
 
-  render() {
-    const validateFields = (err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
+  static propTypes = {
+    actions: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired,
+    router: PropTypes.object.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    // Stock les champs du formulaire de login dans l'état local
+    this.state = {
+      fields: {
+        email: {
+          value: '',
+        },
+        password: {
+          value: '',
+        },
+        rememberMe: {
+          value: false,
+        }
       }
     };
+  }
 
+  componentWillMount() {
+    const remember = localStorage.getItem('auth.remember');
+
+    // Initialise l'addresse e-mail et la case à cocher si l'utilisateur à décidé de s'en souvenir
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        email: {
+          value: remember ? remember : ''
+        },
+        rememberMe: {
+          value: !!remember
+        }
+      }
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+    // Redirige sur la route principale si l'utilisateur s'est authentifié
+    if (nextProps.auth.loggedIn) {
+      this.props.router.push('/');
+    }
+  }
+
+  /**
+   * Met à jour la valeur des champs modifiés dans l'état local.
+   */
+  handleFormChange = (changedFields) => {
+    this.setState({
+      fields: { ...this.state.fields, ...changedFields },
+    });
+  }
+
+  /**
+   * Déclenche l'action de "login" lorsque le formulaire est submit.
+   */
+  onSubmit = () => {
+    const { email, password, rememberMe } = this.state.fields;
+    this.props.actions.login(email.value, password.value, rememberMe.value);
+  }
+
+  render() {
     return (
-      <LoginFormComponent validateFields={validateFields} />
+      <LoginForm {...this.state.fields}
+        errors={this.props.auth.loginErrors}
+        onChange={this.handleFormChange}
+        onSubmit={this.onSubmit}
+        isLoggingIn={this.props.auth.isLoggingIn} />
     );
   }
 }
--- src/client/assets/javascripts/features/auth/components/login/LoginForm.js
+++ src/client/assets/javascripts/features/auth/components/login/LoginForm.js
@@ -1,48 +1,94 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Alert, Form, Icon, Input, Button, Checkbox } from 'antd';
 const FormItem = Form.Item;
 
+const LoginForm = Form.create({
+
+  onFieldsChange(props, changedFields) {
+    props.onChange(changedFields);
+  },
+
+  mapPropsToFields(props) {
+    return {
+      email: {
+        ...props.email
+      },
+      password: {
+        ...props.password
+      },
+      rememberMe: {
+        ...props.rememberMe
+      }
+    };
+  },
+})(
 class LoginForm extends Component {
 
   static propTypes = {
+    errors: PropTypes.array,
     form: PropTypes.object.isRequired,
-    validateFields: PropTypes.func.isRequired,
+    isLoggingIn: PropTypes.bool.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    validateFields: PropTypes.func,
   };
 
-  handleSubmit = (e) => {
+  /**
+   * Lorsque le formulaire est submit, la validité des champs est vérifiée et si
+   * tout est convenable, le callback provenant des props est exécuté.
+   */
+  onSubmit = (e) => {
     e.preventDefault();
-    this.props.form.validateFields(this.props.validateFields);
+
+    this.props.form.validateFieldsAndScroll((err) => {
+      if (!err) {
+        this.props.onSubmit();
+      }
+    });
   }
 
   render() {
     const { getFieldDecorator } = this.props.form;
+
+    const errorMessages = this.props.errors && this.props.errors.length > 0 && (
+      <ul>
+        {this.props.errors.map(function(error, index) {
+          return <li key={index}>{error}</li>;
+        })}
+      </ul>
+    );
+
     return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <h1>My Acess Web</h1>
+      <Form onSubmit={this.onSubmit} className="login-form">
+        <h1>Connexion</h1>
+        {errorMessages && (
+          <Alert type="error" message="Echec de la connexion" description={errorMessages} closable />
+        )}
+
         <FormItem>
           {getFieldDecorator('email', {
-            rules: [{ required: true, message: 'Veuillez entrer votre adresse emai!' }],
+            rules: [{ required: true, message: 'Veuillez entrer votre adresse e-mail.' }],
           })(
-            <Input addonBefore={<Icon type="user" />} placeholder="Email" />
+            <Input addonBefore={<Icon type="user" />} placeholder="Addresse e-mail" />
           )}
         </FormItem>
+
         <FormItem>
           {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Veuillez entrer votre mot de passe!' }],
+            rules: [{ required: true, message: 'Veuillez entrer votre mot de passe.' }],
           })(
-            <Input addonBefore={<Icon type="lock" />} type="password" placeholder="Password" />
+            <Input addonBefore={<Icon type="lock" />} type="password" placeholder="Mot de passe" />
           )}
         </FormItem>
+
         <FormItem>
-          {getFieldDecorator('remember', {
+          {getFieldDecorator('rememberMe', {
             valuePropName: 'checked',
-            initialValue: true,
           })(
-            <Checkbox>Rester connecté</Checkbox>
+            <Checkbox>Se souvenir de moi</Checkbox>
           )}
-          <a className="login-form-forgot">Mot de passe oublié ?</a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
+          <Link to="/lost-password" className="login-form-forgot">Mot de passe oublié ?</Link>
+          <Button type="primary" htmlType="submit" className="login-form-button" loading={this.props.isLoggingIn}>
             Connexion
           </Button>
           Ou <Link to="/register">créer un compte!</Link>
@@ -50,6 +96,6 @@ class LoginForm extends Component {
       </Form>
     );
   }
-}
+});
 
-export const LoginFormComponent = Form.create()(LoginForm);
+export default LoginForm;
