171cb7248dcbdbbc23fcae3b089145bda6427008
Benjamin Ganty <benjaminganty@msn.com>
2017-03-06 18:57:15 +0100

    refactor(SideMenu): modifie le comportement du SideMenu
    Le menu gère lui-même quel item est sélectionné et quels sous-menus sont ouvert en fonction de la
    route. Lorsqu'on défini un Item dans le menu, on peut lui passer le prop "isRouterLink" pour que
    celui-ci se comporte comme un lien de router. Par défaut, c'est la propriété "key" qui est
    considérée comme étant le chemin du lien, mais il est possible d'overide "key" avec le prop "to"
    (toujours sur l'Item).

--- src/client/assets/javascripts/features/core/components/SideMenu/SideMenu.js
+++ src/client/assets/javascripts/features/core/components/SideMenu/SideMenu.js
@@ -1,66 +1,165 @@
 import React, { Component, PropTypes } from 'react';
+import { withRouter } from 'react-router';
 import { Icon, Menu } from 'antd';
-const { SubMenu } = Menu;
-import { Link } from 'react-router';
+const { Item, SubMenu } = Menu;
 
 import './SideMenu.scss';
 
+@withRouter
 export default class SideMenu extends Component {
 
-  static contextTypes = {
-      router: React.PropTypes.object
+  static propTypes = {
+    collapsed: PropTypes.bool,
+    location: PropTypes.object.isRequired,
+    router: PropTypes.object.isRequired,
   };
 
-  render() {
-    var defaultOpenKeys=[];
-    var defaultSelectedKeys=[];
-    if (this.context.router.isActive('display-management')) {
-      defaultOpenKeys.push("display-management");
-      if (this.context.router.isActive('display-management/file')) {
-        defaultOpenKeys.push("display-management-media-library");
-        defaultSelectedKeys.push("display-management/file");
-      } else if (this.context.router.isActive('display-management/scene')) {
-        defaultOpenKeys.push("display-management-media-library");
-        defaultSelectedKeys.push("display-management/scene");
-      } else if (this.context.router.isActive('display-management/screen')) {
-        defaultSelectedKeys.push("display-management/display");
-      } else if (this.context.router.isActive('display-management/agenda')) {
-        defaultSelectedKeys.push("display-management/agenda");
-      }
+  constructor() {
+    super();
+
+    this.state = {
+      openKeys: [],
+      selectedKeys: [],
+    };
+  }
+
+  /**
+   * Lorsque le menu s'est affiché, définit les "openKeys" et la "selectedKeys" en fonction de
+   * "pathname" (la route) et de si le menu est "collapsed" ou non.
+   */
+  componentDidMount() {
+    const keys = this.findKeys(this.props.location.pathname);
+
+    this.setState({
+      selectedKeys: keys.selectedKeys,
+      openKeys: this.props.collapsed ? [] : keys.openKeys
+    });
+  }
+
+  /**
+   * Si la route (pathname) change, adapte la "selectedKeys".
+   * Si le menu change de format (collapsed), adapte les "openKeys".
+   */
+  componentWillReceiveProps(nextProps) {
+    const { pathname } = nextProps.location;
+
+    if (pathname != this.props.location.pathname || nextProps.collapsed != this.props.collapsed) {
+      const keys = this.findKeys(pathname);
+
+      this.setState({
+        selectedKeys: keys.selectedKeys,
+        openKeys: nextProps.collapsed ? [] : keys.openKeys
+      });
     }
+  }
+
+  /**
+   * Cherche dans le menu la "selectedKeys" et les "openKeys".
+   */
+  findKeys(pathname) {
+
+    // Utilise une fonction récursive pour récupérer les "keys" dans le menu
+    let keys = (function findOpenKeysRec(menuNode) {
+
+      // Si le noeud est un item avec le prop "isRouterLink" valant "true"
+      if (menuNode.props && menuNode.props.isRouterLink) {
+        // Si le chemin du lien correspond à la route (pathname), la "key" de
+        // l'item est retournée en tant que "selectedKeys"
+        const linkPathReg = new RegExp('^' + (menuNode.props.to || menuNode.key));
+        if (pathname.match(linkPathReg)) {
+          return {
+            selectedKeys: [ menuNode.key ],
+            openKeys: []
+          };
+        }
+      }
+      // Sinon, si le noeud a des enfants
+      else if (menuNode.props && menuNode.props.children) {
+        const children = menuNode.props.children instanceof Array ? menuNode.props.children : [ menuNode.props.children ];
+
+        // Explore les enfants en appelant récursivement cette fonction
+        for (let child of children) {
+          let keys = findOpenKeysRec(child);
+
+          // Si des clés sont remontées de la branche enfant, l'item sélectionné a donc été trouvé dans celle-ci
+          if (keys) {
+            // Ajoute la key du noeud courant (s'il y en a une) aux openKeys et les retourne
+            if (menuNode.key) {
+              keys.openKeys = [ menuNode.key, ...keys.openKeys ];
+            }
+            return keys;
+          }
+        }
+      }
+      // Si rien n'a été trouvé, retourne null
+      return null;
+
+    })(this.menu);
+
+    return keys ? keys : { selectedKeys: [], openKeys: [] };
+  }
+
+  /**
+   * Callback déclenché par une ouverture / fermeture d'un sous-menu.
+   * Stoque les "openKeys" dans l'état local.
+   */
+  onOpenChange = (openKeys) => {
+    this.setState({
+      openKeys
+    });
+  }
+
+  /**
+   * Callback déclenché par un clic sur un item du menu.
+   * Si l'item a le prop "to" définit, sa valeur est utilisée pour la navigation, sinon c'est sa "key".
+   */
+  onCLick = (target) => {
+    if (target.item.props.isRouterLink) {
+      this.props.router.push(target.item.props.to || target.key);
+    }
+  }
+
+  render() {
 
     return (
-      <Menu id="SideMenu"
-        defaultOpenKeys={defaultOpenKeys}
-        defaultSelectedKeys={defaultSelectedKeys}
+      <Menu
+        id="SideMenu"
+        ref={(ref) => this.menu = ref}
+        mode={this.props.collapsed ? 'vertical' : 'inline'}
         className={this.props.collapsed ? 'collapsed' : ''}
-        mode={this.props.collapsed ? 'vertical' : 'inline'}>
+        openKeys={this.state.openKeys}
+        selectedKeys={this.state.selectedKeys}
+        onOpenChange={this.onOpenChange}
+        onClick={this.onCLick}>
 
-        <Menu.Item key="dashboard">
-          <Icon type="windows" />
-          <span className="item-text">Dashboard</span>
-        </Menu.Item>
+        { /*
+        <Item key="/dashboard" isRouterLink>
+          <Icon type="windows" /><span className="hidden-if-collapsed">Dashboard</span>
+        </Item>
+        */ }
+        <SubMenu
+          key="display-management"
+          title={<span><Icon type="video-camera" /><span className="hidden-if-collapsed">Afficheurs</span></span>}>
 
-        <SubMenu key="display-management" title={<span><Icon type="video-camera" /><span className="item-text">Afficheurs</span></span>}>
           <SubMenu key="display-management-media-library" title={<span><Icon type='file' />Médiathèque</span>}>
 
-            <Menu.Item key="display-management/file">
-              <Link to="/display-management/file"><Icon type="picture" />Fichiers</Link>
-            </Menu.Item>
+            <Item key="/display-management/file" isRouterLink>
+              <Icon type="picture" />Fichiers
+            </Item>
 
-            <Menu.Item key="display-management/scene">
-              <Link to="/display-management/scene"><Icon type="appstore-o" />Scènes</Link>
-            </Menu.Item>
+            <Item key="/display-management/scene" isRouterLink>
+              <Icon type="appstore-o" />Scènes
+            </Item>
 
           </SubMenu>
 
-          <Menu.Item key="display-management/screen">
-            <Link to="/display-management/screen"><Icon type="desktop" />Écrans</Link>
-          </Menu.Item>
+          <Item key="/display-management/screen" isRouterLink>
+            <Icon type="desktop" />Écrans
+          </Item>
 
-          <Menu.Item key="display-management/agenda">
-            <Link to="/display-management/agenda"><Icon type="calendar" />Agendas</Link>
-          </Menu.Item>
+          <Item key="/display-management/agenda" isRouterLink>
+            <Icon type="calendar" />Agendas
+          </Item>
 
         </SubMenu>
 
@@ -68,7 +167,3 @@ export default class SideMenu extends Component {
     );
   }
 }
-
-SideMenu.propTypes = {
-  collapsed: PropTypes.bool
-};
