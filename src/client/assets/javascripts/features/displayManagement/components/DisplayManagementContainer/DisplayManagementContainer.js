1349d04374506f6a03ba28fd16e834f1f00b6da0
Brian <briannydegger@gmail.com>
2017-03-23 14:11:54 +0100

    refactor(DisplayManagement): supprime le breadcrump / fil

--- src/client/assets/javascripts/features/displayManagement/components/DisplayManagementContainer/DisplayManagementContainer.js
+++ src/client/assets/javascripts/features/displayManagement/components/DisplayManagementContainer/DisplayManagementContainer.js
@@ -1,54 +1,12 @@
 import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router';
-import { Layout, Breadcrumb, Icon } from 'antd';
+import { Layout} from 'antd';
 
 export default class DisplayManagementContainer extends Component {
 
-  pathConnection(path) {
-    switch (path) {
-      case 'file':
-        return 'Fichiers';
-      case 'scene':
-        return 'Scènes';
-      case 'screen':
-        return 'Écrans';
-      case 'agenda':
-        return 'Agendas';
-      default:
-        return path;
-    }
-  }
-
   render() {
-    const paths = location.pathname.split('/');
-    var pathsView = [];
-    for (var i=0; i < paths.length; i++) {
-      if (paths[i] !== 'display-management' && paths[i] !== '')
-        pathsView.push(
-          <Breadcrumb.Item key={paths[i]}>
-            { (i < 3) ?
-              <Link to={'/display-management/' + paths[i]}> {this.pathConnection(paths[i])} </Link>
-              :
-              <span> {this.pathConnection(paths[i])} </span>
-            }
-          </Breadcrumb.Item>
-        );
-    }
 
     return (
       <Layout id={'displayManagementLayout'}>
-        <Layout.Header>
-          <Breadcrumb>
-            <Breadcrumb.Item>
-              <Icon type="home" />
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              <Icon type="video-camera" />
-              <span>Afficheurs</span>
-            </Breadcrumb.Item>
-            {pathsView}
-          </Breadcrumb>
-        </Layout.Header>
         <Layout.Content style={{overflowY: 'scroll'}}>
           {this.props.children}
         </Layout.Content>
