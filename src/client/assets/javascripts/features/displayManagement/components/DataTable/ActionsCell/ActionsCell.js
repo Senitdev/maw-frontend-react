0f967ee61aa714ecae1ad285fd8c20c86d80cd85
Balthazar de Moncuit <balthazar.de.moncuit@gmail.com>
2017-04-28 14:56:31 +0200

    feat(DataTable): Ajoute les bouttons d'action optionnel: 'edition d'agenda' & 'edition horraire d'activité'

--- src/client/assets/javascripts/features/displayManagement/components/DataTable/ActionsCell/ActionsCell.js
+++ src/client/assets/javascripts/features/displayManagement/components/DataTable/ActionsCell/ActionsCell.js
@@ -4,14 +4,16 @@ import { Button, Popconfirm, Tooltip } from 'antd';
 export default class ActionsCell extends Component {
 
   static propTypes = {
+    onCalendar: PropTypes.func,
     onDelete: PropTypes.func,
     onEdit: PropTypes.func,
     onPreview: PropTypes.func,
+    onSchedule: PropTypes.func,
     record: PropTypes.object.isRequired,
   };
 
   render() {
-    const { onDelete, onEdit, onPreview, record } = this.props;
+    const { onCalendar,onDelete, onEdit, onPreview, onSchedule, record } = this.props;
     return (
       <div className="maw-actions-cell">
         { onPreview &&
@@ -21,6 +23,14 @@ export default class ActionsCell extends Component {
         { onEdit &&
         <Tooltip title="Modifier" placement="bottom" mouseEnterDelay={0.6}>
           <Button icon="edit" onClick={() => onEdit(record)} />
+        </Tooltip> }
+        { onSchedule &&
+        <Tooltip title="Horraires d'activité" placement="bottom" mouseEnterDelay={0.6}>
+          <Button icon="clock-circle-o" onClick={() => onSchedule(record)} />
+        </Tooltip> }
+        { onCalendar &&
+        <Tooltip title="Agenda" placement="bottom" mouseEnterDelay={0.6}>
+          <Button icon="calendar" onClick={() => onCalendar(record)} />
         </Tooltip> }
         { onDelete &&
         <Tooltip title="Supprimer" placement="bottom" mouseEnterDelay={0.6}>
